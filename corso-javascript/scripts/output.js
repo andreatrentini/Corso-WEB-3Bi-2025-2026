c25b72a5cb44ef413a7f4f40abf9467b939b9290
andreatrentini <55395467+andreatrentini@users.noreply.github.com>
2026-02-20 09:28:56 +0000

    FEAT: inizio corso javascript

---
+++ corso-javascript/scripts/output.js
@@ -0,0 +1,3 @@
+console.log('x: ', x);
+console.log('nome: ', nome);
+console.log('finito:', finito);
---
+++ corso-javascript/scripts/variabili.js
@@ -0,0 +1,17 @@
+// Dichiarazione di una varibile di tipo numero
+var x = 5;
+// Dichiarazione di una varibile di tipo string
+var nome = 'Matteo';
+// Dichiarazione di una varisabile di tipo boolean
+var finito = false;
+
+// Possibile ma da non fare...
+var vettore = ['Mario', 2, true];
+
+
+
+vettore.push(9);
+
+vettore[0] = 'Mattia';
+
+console.log(vettore);
