4840cc6f42866a0f7e7889182200792a81fe969d
andreatrentini <55395467+andreatrentini@users.noreply.github.com>
2026-04-17 08:25:44 +0000

    FEAT: modifica del dom - Saluta

---
+++ corso-javascript/scripts/dom.js
@@ -0,0 +1,11 @@
+function saluta() {
+    // 1. istanzio un oggetto che rappresenta nel DOM il tag div nel quale scrivere il saluto
+    var divSaluto = document.getElementById('saluto');
+    // 2. Modifico le caratteristiche del tag scelto
+    divSaluto.innerText = 'Benvenuto nel corso Javascript!';
+}
+
+function benvenuto() {
+    // 1. recuperare il nome scritto dall'utnte nella input
+    var nome = document.getElementById()
+}
